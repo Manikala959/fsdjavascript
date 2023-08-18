63e0e55048b411384b3004ab5730a9401a244eeb
Manikala959 <manikala959@gmail.com>
2023-08-18 20:56:57 +0530

    javascript

---
+++ intr.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+     <meta charset="utf-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1">
+     <title></title>
+  //1.window alert
+  //alert("Success.");
+  //2.confirmation box
+  var c=confirm("Are you sure delete the record");
+  document.write(c);
+</script>
+</body>
+</html>
